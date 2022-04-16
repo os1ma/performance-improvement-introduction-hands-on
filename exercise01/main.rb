4a66ccbb2661b851550ef3af52d4f8a16f082b89
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-04-16 20:43:55 +0900

    Add MySQL connect sample

--- exercise01/main.rb
+++ exercise01/main.rb
@@ -1,9 +1,23 @@
+require 'mysql2'
+
 def sum(a, b)
   a + b
 end
 
 def main
-  puts 'hello'
+  client = Mysql2::Client.new(
+    :host => '127.0.0.1',
+    :database => 'mydb',
+    :username => 'myuser',
+    :password => 'mypassword'
+  )
+
+  results = client.query('select * from users')
+  results.each do |row|
+    puts row
+  end
+
+  client.close
 end
 
 if __FILE__ == $0
