062e86c04ad48c208f47aa2d2df27e2e11c0927b
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-04-19 21:56:10 +0900

    Implement exercise01

--- exercise01/main.rb
+++ exercise01/main.rb
@@ -1,10 +1,17 @@
 require 'mysql2'
 
-def sum(a, b)
-  a + b
+class PostWithTagNames
+  attr_reader :post_id, :title, :content, :tag_names
+
+  def initialize(post_id, title, content, tag_names)
+    @post_id = post_id
+    @title = title
+    @content = content
+    @tag_names = tag_names
+  end
 end
 
-def main
+def find_post_with_tag_names_array_by_user_id(user_id)
   client = Mysql2::Client.new(
     :host => '127.0.0.1',
     :database => 'mydb',
@@ -12,12 +19,31 @@ def main
     :password => 'mypassword'
   )
 
-  results = client.query('select * from users')
-  results.each do |row|
-    puts row
+  posts_stmt = client.prepare('select id, title, content from posts where user_id = ?')
+  posts = posts_stmt.execute(user_id)
+  post_with_tag_names_array = posts.map do |post|
+    post_id = post['id']
+    title = post['title']
+    content = post['content']
+
+    taggingsStmt = client.prepare('select post_id, tag_id from taggings where post_id = ?')
+    taggings = taggingsStmt.execute(post_id)
+    tag_names = taggings.flat_map do |tagging|
+      tag_id = tagging['tag_id']
+
+      tagsStmt = client.prepare('select id, name from tags where id = ?')
+      tags = tagsStmt.execute(tag_id)
+      tags.map do |tag|
+        tag['name']
+      end
+    end
+
+    PostWithTagNames.new(post_id, title, content, tag_names)
   end
 
   client.close
+
+  return post_with_tag_names_array
 end
 
 if __FILE__ == $0
--- exercise01/main_test.rb
+++ exercise01/main_test.rb
@@ -2,7 +2,13 @@ require 'minitest/autorun'
 require './main'
 
 class SampleTest < Minitest::Test
-  def test_sum
-    assert_equal 3, sum(1, 2)
+  def test_find_post_with_tag_names_array_by_user_id
+    actual = find_post_with_tag_names_array_by_user_id(1)
+
+    assert_equal 1, actual[0].post_id
+    assert_equal 2, actual[1].post_id
+    assert_equal 3, actual[2].post_id
+    assert_equal 4, actual[3].post_id
+    assert_equal 5, actual[4].post_id
   end
 end
