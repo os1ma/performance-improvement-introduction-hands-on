480bbf17a5e5380490c499b2e55fe66572df3286
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-04-19 19:53:20 +0900

    Update DDL

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,6 +5,23 @@ create table `users` (
   `name` varchar(255) not null
 );
 
+create table `posts` (
+  `id` int primary key auto_increment,
+  `user_id` int not null,
+  `title` varchar(255) not null,
+  `content` text not null
+);
+
+create table `tags` (
+  `id` int primary key auto_increment
+);
+
+create table `taggings` (
+  `post_id` int not null,
+  `tag_id` int not null,
+  primary key(`post_id`, `tag_id`)
+);
+
 insert into `users` (`name`) values
 ('Alice'),
 ('Bob');
